732f5e498e51776f7918564c5cf1ebcdc65a6b54
Mariola Podoba <mariolapodoba@MacBook-Air-Mariola.local>
2020-06-15 01:11:40 +0200

    CRUD and login

--- app/src/Form/PostType.php
+++ app/src/Form/PostType.php
@@ -10,6 +10,7 @@ use App\Form\DataTransformer\TagDataTransformer;
 use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -63,7 +64,7 @@ class PostType extends AbstractType
         );
         $builder->add(
             'text',
-            TextType::class,
+            TextareaType::class,
             [
                 'label' => 'text',
                 'required' => true,
